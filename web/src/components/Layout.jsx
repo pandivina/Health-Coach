3168840dd330a588035055766592590c740003e2
pandivina <pandivinatheflowershop@gmail.com>
2026-05-16 16:00:31 +0200

    Add files via upload

--- web/src/components/Layout.jsx
+++ web/src/components/Layout.jsx
@@ -1,1 +1,13 @@
+import { Outlet } from 'react-router-dom'
+import BottomNav from './BottomNav'
 
+export default function Layout() {
+  return (
+    <div className="min-h-screen bg-[#0a0a12] flex flex-col max-w-lg mx-auto relative">
+      <main className="flex-1 overflow-y-auto">
+        <Outlet />
+      </main>
+      <BottomNav />
+    </div>
+  )
+}

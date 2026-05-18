3fd747f20677910bdf84d73eca1b9dc919d8cbcc
pandivina <pandivinatheflowershop@gmail.com>
2026-05-19 00:33:49 +0200

    Add files via upload

--- web/src/components/Layout.jsx
+++ web/src/components/Layout.jsx
@@ -1,9 +1,12 @@
 import { Outlet } from 'react-router-dom'
 import BottomNav from './BottomNav'
+import { useTheme } from '../contexts/ThemeProvider'
 
 export default function Layout() {
+  const { theme } = useTheme()
   return (
-    <div className="min-h-screen bg-[#0a0a12] flex flex-col max-w-lg mx-auto relative">
+    <div className="min-h-screen flex flex-col max-w-lg mx-auto relative"
+      style={{ background: theme.bg }}>
       <main className="flex-1 overflow-y-auto">
         <Outlet />
       </main>

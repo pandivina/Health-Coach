6311ebd825f76fae3ffe8b47be5bc3f057704b84
pandivina <pandivinatheflowershop@gmail.com>
2026-05-19 22:29:55 +0200

    Reorganize component structure in main.jsx

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -2,19 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { ThemeProvider } from './contexts/ThemeProvider'
-import './index.css'
 import { GuidedTourProvider } from './contexts/GuidedTourProvider'
 import SpotlightOverlay from './components/tour/SpotlightOverlay'
 import GuidedTour from './components/tour/GuidedTour'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
-  <GuidedTourProvider>
-    <App />
-    <SpotlightOverlay />
-    <GuidedTour />
-  </GuidedTourProvider>
-</ThemeProvider>
+      <GuidedTourProvider>
+        <App />
+        <SpotlightOverlay />
+        <GuidedTour />
+      </GuidedTourProvider>
+    </ThemeProvider>
   </React.StrictMode>
 )

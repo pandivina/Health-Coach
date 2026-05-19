625a3c2f594c1147ff56bf0f3f05108a38d53b37
pandivina <pandivinatheflowershop@gmail.com>
2026-05-19 19:03:48 +0200

    Integrate GuidedTourProvider and related components

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { ThemeProvider } from './contexts/ThemeProvider'
 import './index.css'
+import { GuidedTourProvider } from './contexts/GuidedTourProvider'
+import SpotlightOverlay from './components/tour/SpotlightOverlay'
+import GuidedTour from './components/tour/GuidedTour'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
-      <App />
-    </ThemeProvider>
+  <GuidedTourProvider>
+    <App />
+    <SpotlightOverlay />
+    <GuidedTour />
+  </GuidedTourProvider>
+</ThemeProvider>
   </React.StrictMode>
 )

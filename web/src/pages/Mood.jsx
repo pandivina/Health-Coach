5f833417895b96583c6cb854a86dd3dce588b0c1
pandivina <pandivinatheflowershop@gmail.com>
2026-05-20 19:34:27 +0200

    Update Mood.jsx

--- web/src/pages/Mood.jsx
+++ web/src/pages/Mood.jsx
@@ -395,7 +395,7 @@ function PandaFrame({ running }) {
         ? { duration: 2.2, repeat: Infinity, ease: 'easeInOut' }
         : { duration: 0.3 }
       }
-      style={{ width: 150, height: 150 }}>
+      style={{ width: 300, height: 300 }}>
       <AnimatePresence mode="wait">
         <motion.img
           key={frame}

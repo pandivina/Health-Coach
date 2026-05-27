db739c226c12646e0d0cd1f592fcfa01c2984a3e
pandivina <pandivinatheflowershop@gmail.com>
2026-05-27 10:49:28 +0200

    Update RecetasTab.jsx

--- web/src/components/nutrition/RecetasTab.jsx
+++ web/src/components/nutrition/RecetasTab.jsx
@@ -1,14 +1,1 @@
-import PremiumGate from '../components/PremiumGate'
-
-// Estado:
-const [showPremium, setShowPremium] = useState(false)
-
-// En el catch:
-} catch (err) {
-  if (err.message === 'premium_required') setShowPremium(true)
-  else setError('No se pudo analizar...')
-}
-
-// En el JSX al final:
-<PremiumGate visible={showPremium} onClose={() => setShowPremium(false)} feature="Análisis de foto" />
 export { RecetasTab as default } from './NutritionComponents'

ea2401d033949158606989d3bfbcf47d604dfd34
pandivina <pandivinatheflowershop@gmail.com>
2026-05-27 14:51:31 +0200

    Update main.jsx

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -6,15 +6,18 @@ import { GuidedTourProvider } from './contexts/GuidedTourProvider'
 import SpotlightOverlay from './components/tour/SpotlightOverlay'
 import GuidedTour from './components/tour/GuidedTour'
 import './index.css'
+import { LanguageProvider } from './contexts/LanguageContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
-      <GuidedTourProvider>
+      <LanguageProvider>
+          <GuidedTourProvider>
         <App />
         <SpotlightOverlay />
         <GuidedTour />
-      </GuidedTourProvider>
-    </ThemeProvider>
+    </GuidedTourProvider>
+  </LanguageProvider>
+</ThemeProvider>
   </React.StrictMode>
 )

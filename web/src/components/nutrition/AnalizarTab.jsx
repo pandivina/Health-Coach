c2ae97aeb70052ea24ea9ec15adf76122afe4c00
pandivina <pandivinatheflowershop@gmail.com>
2026-05-27 13:21:35 +0200

    Update AnalizarTab.jsx

--- web/src/components/nutrition/AnalizarTab.jsx
+++ web/src/components/nutrition/AnalizarTab.jsx
@@ -1,14 +1,1 @@
-import PremiumGate from '../components/PremiumGate'
 export { AnalizarTab as default } from './NutritionTabs2'
-
-// Estado:
-const [showPremium, setShowPremium] = useState(false)
-
-// En el catch:
-} catch (err) {
-  if (err.message === 'premium_required') setShowPremium(true)
-  else setError('No se pudo analizar...')
-}
-
-// En el JSX al final:
-<PremiumGate visible={showPremium} onClose={() => setShowPremium(false)} feature="Análisis de foto" />

08f64c739d5ccefa5548d358adc8faf3fb41e346
pandivina <pandivinatheflowershop@gmail.com>
2026-06-06 19:16:48 +0200

    Adjust panda_baby image position in OnboardingDemo

--- web/src/pages/OnboardingDemo.jsx
+++ web/src/pages/OnboardingDemo.jsx
@@ -147,7 +147,7 @@ export default function OnboardingDemo() {
               <img src="/panda/panda_baby.png" alt=""
                 style={{
                   position:'absolute',
-                  top:'65%', left:'28%', width:'44%', height:'38%',
+                  top:'35%', left:'28%', width:'44%', height:'38%',
                   objectFit:'contain', zIndex:2,
                 }}
                 onError={()=>setImgErrs(e=>({...e,baby_inner:true}))}

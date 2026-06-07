62bef6f4059be29b4d16d7c52899aa639911e598
pandivina <pandivinatheflowershop@gmail.com>
2026-06-07 22:58:42 +0200

    Update OnboardingDemo.jsx

--- web/src/pages/OnboardingDemo.jsx
+++ web/src/pages/OnboardingDemo.jsx
@@ -123,7 +123,7 @@ export default function OnboardingDemo() {
                 width:'100vw',
                 height:'150vw',
                 flexShrink:15,
-                marginTop:'15%',
+                marginTop:'0%',
                   }}
                   onError={()=>setImgErrs(e=>({...e,[`f${i}`]:true}))}
                 />
